e60f6965c782112bce834c6280f7087b79652b21
Luzeq123 <morkovkin465pro@gmail.com>
2021-09-27 11:02:50 +0700

    ver от путина

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,7 @@ fun main()
 {
     Task0()
     Task1()
+    Task2()
 }
 
 fun Task0()
@@ -11,4 +12,10 @@ fun Task0()
 fun Task1()
 {
     print("Hello World!")
+
+}
+
+fun Task2() {
+    print("Путин верни несквик с пивом!")
+
 }
