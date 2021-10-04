38f4a5b02313598d4c0241c8330f3ba832fccc02
Luzeq123 <morkovkin465pro@gmail.com>
2021-10-04 11:09:37 +0700

    if8

--- src/main.kt
+++ src/main.kt
@@ -16,6 +16,10 @@ fun main() {
             "b1" -> Begin1()
 
             "b4" -> Begin4()
+            "if5" -> if5()
+            "if6" -> if6()
+            "if7" -> if7()
+            "if8" -> if8()
 
             "b7" -> Begin7()
             else -> print("Такой задачи в списке нет!")
@@ -51,20 +55,22 @@ fun main() {
 
 
 fun Info() {
+
+    var timing = 200
     print("Бобро пожаловать в консольную версию задачника с примерами!\n")
-    Thread.sleep(500)
-    print ("y - кнопка отвечающая за ответ \"да\"\n")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
+    print("y - кнопка отвечающая за ответ \"да\"\n")
+    Thread.sleep(timing.toLong())
     print("n - кнопка отвечающая за ответ \"нет\"\n")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
     print("Код задачи определяется ее первой буквой в названии\n")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
     print("+ порядковый номер в списке.\n")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
     print("Например, Begin1 - b1.\n")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
     print("После ввода ответа нужно нажать клавишу Enter!")
-    Thread.sleep(500)
+    Thread.sleep(timing.toLong())
 }
 
 fun Begin1() {
@@ -93,10 +99,12 @@ fun Begin4() {
 }
 
 
-fun Begin7()
-{
-println("Найти длину окружности L и площадь круга S заданного радиуса R:\n" +
-        "L = 2·π·R, S = π·R^2")
+fun Begin7() {
+    println(
+        "Найти длину окружности L и площадь круга S заданного радиуса R:\n" +
+                "L = 2·π·R, S = π·R^2"
+    )
+    print("Введите радиус R:")
 
 
     val r: Double = Scanner(System.`in`).nextDouble()
@@ -105,10 +113,118 @@ println("Найти длину окружности L и площадь круг
     println("L = $l")
     println("S = $s")
 
+}
+
+fun if5() {
+
+
+    println("Даны три целых числа. Найти количество положительных и количество\n + отрицательных чисел в исходном наборе")
+
+    print("введите певрое число")
+    var a = readLine()!!.toDouble()
+    print("введите второе число")
+    var b = readLine()!!.toDouble()
+
+    print("введите третье число")
+    var c = readLine()!!.toDouble()
+
+    var plus = 0
+    var minus = 0
+    var ravno = 0
+
+
+    if (a > 0) {
+        plus++
+    }
+    if (b > 0) {
+        plus++
+    }
+    if (c > 0) {
+        plus++
+    }
+    if (a == 0.0) {
+        ravno++
+    }
+    if (b == 0.0) {
+        ravno++
+    }
+    if (c == 0.0) {
+        ravno++
+    }
+    if (a < 0) {
+        minus++
+    }
+    if (b < 0) {
+        minus++
+    }
+    if (c < 0) {
+        minus++
+    }
+
+    println("Количество положительных чисел - $plus")
+    println("Количество отрицательных чисел - $minus")
+    println("Количество нулёв - $ravno")
 
 }
 
+fun if6(){
 
+println("Даны два числа. Вывести большее из них.")
+println("Введите первое число ")
+    var a = readLine()!!.toDouble()
+    println("Введите второе число ")
+    var b = readLine()!!.toDouble()
+
+
+if (a > b)
+println("число $a больше")
+
+    if (a < b)
+        println("число $b больше")
+
+
+}
+
+fun if7(){
+
+print("Даны два числа. Вывести порядковый номер меньшего из них.")
+    println("веддите первое число ")
+    var a = readLine()!!.toDouble()
+    println("веддите второе число ")
+    var b = readLine()!!.toDouble()
+
+
+    if (a > b)
+        println("первое больше второго")
+
+    if (a < b)
+        println("второе больше первого")
+    if (a == b)
+        println("числа равны")
+}
+
+fun if8(){
+
+    print("Даны два числа. Вывести вначале большее, а затем меньшее из них.")
+
+
+    println("Введите первое число ")
+    var a = readLine()!!.toDouble()
+    println("Введите второе число ")
+    var b = readLine()!!.toDouble()
+
+
+
+    if (a > b) {
+        println("большее число $a")
+        println("меньшее число $b")
+    }
+    if (a < b) {
+        println("большее число $b")
+        println("меньшее число $a")
+    }
+
+}
 
 
 
